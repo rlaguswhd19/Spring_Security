3adfd8d363a3d6570c8dbcd0ab80ad3a3497d953
rlaguswhd19 <rlaguswhd19@naver.com>
2020-03-20 14:18:01 +0900

    FilterChainProxy

--- Spring_Security/src/main/java/com/hj/security/config/SecurityConfig.java
+++ Spring_Security/src/main/java/com/hj/security/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.hj.security.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,6 +14,7 @@ import com.hj.security.Account.AccountService;
 
 @Configuration
 @EnableWebSecurity
+@Order(Ordered.LOWEST_PRECEDENCE - 50)
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Autowired
@@ -22,7 +25,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
+		http
+			.antMatcher("/**")
+			.authorizeRequests()
 				.mvcMatchers("/", "/info","/accounts/**").permitAll()
 				.mvcMatchers("/admin").hasRole("ADMIN")
 				.anyRequest().authenticated();

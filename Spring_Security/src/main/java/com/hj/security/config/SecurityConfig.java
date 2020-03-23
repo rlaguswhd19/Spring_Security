59fbb29e543f5285821dca157d70dc755608a97a
rlaguswhd19 <rlaguswhd19@naver.com>
2020-03-23 16:07:04 +0900

    AccessDecisionManager Admin > User 설정 AccessDecisionManger Config Handler Config

--- Spring_Security/src/main/java/com/hj/security/config/SecurityConfig.java
+++ Spring_Security/src/main/java/com/hj/security/config/SecurityConfig.java
@@ -1,14 +1,24 @@
 package com.hj.security.config;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.access.AccessDecisionManager;
+import org.springframework.security.access.AccessDecisionVoter;
+import org.springframework.security.access.expression.SecurityExpressionHandler;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.access.vote.AffirmativeBased;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
+import org.springframework.security.web.access.expression.WebExpressionVoter;
 
 import com.hj.security.Account.AccountService;
 
@@ -23,6 +33,30 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Autowired
 	PasswordEncoder passwordEncoder;
 	
+	public AccessDecisionManager accessDecisionManager() {
+		RoleHierarchyImpl roleHierarchyImpl = new RoleHierarchyImpl();
+		roleHierarchyImpl.setHierarchy("ROLE_ADMIN > ROLE_USER");
+		
+		DefaultWebSecurityExpressionHandler handler = new DefaultWebSecurityExpressionHandler();
+		handler.setRoleHierarchy(roleHierarchyImpl);
+		
+		WebExpressionVoter webExpressionVoter = new WebExpressionVoter();
+		webExpressionVoter.setExpressionHandler(handler);
+		
+		List<AccessDecisionVoter<? extends Object>> voters = Arrays.asList(webExpressionVoter);
+		return new AffirmativeBased(voters);
+	}
+	
+	public DefaultWebSecurityExpressionHandler expressionHandler() {
+		RoleHierarchyImpl roleHierarchyImpl = new RoleHierarchyImpl();
+		roleHierarchyImpl.setHierarchy("ROLE_ADMIN > ROLE_USER");
+		
+		DefaultWebSecurityExpressionHandler handler = new DefaultWebSecurityExpressionHandler();
+		handler.setRoleHierarchy(roleHierarchyImpl);
+		
+		return handler;
+	}
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
@@ -30,7 +64,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			.authorizeRequests()
 				.mvcMatchers("/", "/info","/accounts/**").permitAll()
 				.mvcMatchers("/admin").hasRole("ADMIN")
-				.anyRequest().authenticated();
+				.mvcMatchers("/user").hasRole("USER")
+				.anyRequest().authenticated()
+//				.accessDecisionManager(accessDecisionManager())
+				.expressionHandler(securityExpressionHandler());
+				;
 		
 		http.formLogin();
 		http.httpBasic();
